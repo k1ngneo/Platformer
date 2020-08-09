c140d4f15546a159c4e4a39c45cd8340f7f4186e
k1ngneo <k1ng4182k@gmail.com>
2020-08-09 23:26:50 +0200

    added more event defines

--- include/Platformer/app/event_defines.h
+++ include/Platformer/app/event_defines.h
@@ -3,59 +3,124 @@
 
 #include <SDL2/SDL_keycode.h>
 
-#define KB_UNKNOWN SDLK_UNKNOWN
+#define KB_UNKNOWN			SDLK_UNKNOWN
 
-#define KB_BACKSPACE SDLK_BACKSPACE
-#define KB_TAB SDLK_TAB
-#define KB_RETURN SDLK_RETURN
-#define KB_ESCAPE SDLK_ESCAPE
+#define KB_BACKSPACE		SDLK_BACKSPACE
+#define KB_TAB				SDLK_TAB
+#define KB_RETURN			SDLK_RETURN
+#define KB_ESCAPE			SDLK_ESCAPE
 
-#define KB_SPACE SDLK_SPACE
-#define KB_EXCLAIM SDLK_EXCLAIM
-#define KB_QUOTEDBL SDLK_QUOTEDBL
-#define KB_HASH SDLK_HASH
-#define KB_DOLLAR SDLK_DOLLAR
-#define KB_PERCENT SDLK_PERCENT
-#define KB_AMPERSAND SDLK_AMPERSAND
-#define KB_QUOTE SDLK_QUOTE
-#define KB_LEFTPAREN SDLK_LEFTPAREN
-#define KB_RIGHTPAREN SDLK_RIGHTPAREN
+#define KB_SPACE			SDLK_SPACE
+#define KB_EXCLAIM			SDLK_EXCLAIM
+#define KB_QUOTEDBL			SDLK_QUOTEDBL
+#define KB_HASH				SDLK_HASH
+#define KB_DOLLAR			SDLK_DOLLAR
+#define KB_PERCENT			SDLK_PERCENT
+#define KB_AMPERSAND		SDLK_AMPERSAND
+#define KB_QUOTE			SDLK_QUOTE
+#define KB_LEFTPAREN		SDLK_LEFTPAREN
+#define KB_RIGHTPAREN		SDLK_RIGHTPAREN
 
-#define KB_ASTERISK SDLK_ASTERISK
-#define KB_PLUS SDLK_PLUS
-#define KB_COMMA SDLK_COMMA
-#define KB_MINUS SDLK_MINUS
-#define KB_PERIOD SDLK_PERIOD
-#define KB_SLASH SDLK_SLASH
+#define KB_ASTERISK			SDLK_ASTERISK
+#define KB_PLUS				SDLK_PLUS
+#define KB_COMMA			SDLK_COMMA
+#define KB_MINUS			SDLK_MINUS
+#define KB_PERIOD			SDLK_PERIOD
+#define KB_SLASH			SDLK_SLASH
 
-#define KB_CAPSLOCK SDLK_CAPSLOCK
+#define KB_0				SDLK_0
+#define KB_1				SDLK_1
+#define KB_2				SDLK_2
+#define KB_3				SDLK_3
+#define KB_4				SDLK_4
+#define KB_5				SDLK_5
+#define KB_6				SDLK_6
+#define KB_7				SDLK_7
+#define KB_8				SDLK_8
+#define KB_9				SDLK_9
 
-#define KB_F1 SDLK_F1
-#define KB_F2 SDLK_F2
-#define KB_F3 SDLK_F3
-#define KB_F4 SDLK_F4
-#define KB_F5 SDLK_F5
-#define KB_F6 SDLK_F6
-#define KB_F7 SDLK_F7
-#define KB_F8 SDLK_F8
-#define KB_F9 SDLK_F9
-#define KB_F10 SDLK_F10
-#define KB_F11 SDLK_F11
-#define KB_F12 SDLK_F12
+#define KB_COLON			SDLK_COLON
+#define KB_SEMICOLON		SDLK_SEMICOLON
+#define KB_LESS				SDLK_LESS
+#define KB_EQUALS			SDLK_EQUALS
+#define KB_GREATER			SDLK_GREATER
+#define KB_QUESTION			SDLK_QUESTION
+#define KB_AT				SDLK_AT
+#define KB_LEFTBRACKET		SDLK_LEFTBRACKET
+#define KB_BACKSLASH		SDLK_BACKSLASH
+#define KB_RIGHTBRACKET		SDLK_RIGHTBRACKET
+#define KB_CARET			SDLK_CARET
+#define KB_UNDERSCORE		SDLK_UNDERSCORE
+#define KB_BACKQUOTE		SDLK_BACKQUOTE
 
-#define KB_PRINTSCREEN SDLK_PRINTSCREEN
-#define KB_SCROLLLOCK SDLK_SCROLLLOCK
-#define KB_PAUSE SDLK_PAUSE
-#define KB_INSERT SDLK_INSERT
-#define KB_HOME SDLK_HOME
-#define KB_PAGEUP SDLK_PAGEUP
-#define KB_DELETE SDLK_DELETE
-#define KB_END SDLK_END
-#define KB_PAGEDOWN SDLK_PAGEDOWN
+#define KB_A				SDLK_a
+#define KB_B				SDLK_b
+#define KB_C				SDLK_c
+#define KB_D				SDLK_d
+#define KB_E				SDLK_e
+#define KB_F				SDLK_f
+#define KB_G				SDLK_g
+#define KB_H				SDLK_h
+#define KB_I				SDLK_i
+#define KB_J				SDLK_j
+#define KB_K				SDLK_k
+#define KB_L				SDLK_l
+#define KB_M				SDLK_m
+#define KB_N				SDLK_n
+#define KB_O				SDLK_o
+#define KB_P				SDLK_p
+#define KB_Q				SDLK_q
+#define KB_R				SDLK_r
+#define KB_S				SDLK_s
+#define KB_T				SDLK_t
+#define KB_U				SDLK_u
+#define KB_V				SDLK_v
+#define KB_W				SDLK_w
+#define KB_X				SDLK_x
+#define KB_Y				SDLK_y
+#define KB_Z				SDLK_z
 
-#define KB_RIGHT SDLK_RIGHT
-#define KB_LEFT SDLK_LEFT
-#define KB_DOWN SDLK_DOWN
-#define KB_UP SDLK_UP
+#define KB_CAPSLOCK			SDLK_CAPSLOCK
+
+#define KB_F1				SDLK_F1
+#define KB_F2				SDLK_F2
+#define KB_F3				SDLK_F3
+#define KB_F4				SDLK_F4
+#define KB_F5				SDLK_F5
+#define KB_F6				SDLK_F6
+#define KB_F7				SDLK_F7
+#define KB_F8				SDLK_F8
+#define KB_F9				SDLK_F9
+#define KB_F10				SDLK_F10
+#define KB_F11				SDLK_F11
+#define KB_F12				SDLK_F12
+
+#define KB_PRINTSCREEN		SDLK_PRINTSCREEN
+#define KB_SCROLLLOCK		SDLK_SCROLLLOCK
+#define KB_PAUSE			SDLK_PAUSE
+#define KB_INSERT			SDLK_INSERT
+#define KB_HOME				SDLK_HOME
+#define KB_PAGEUP			SDLK_PAGEUP
+#define KB_DELETE			SDLK_DELETE
+#define KB_END				SDLK_END
+#define KB_PAGEDOWN			SDLK_PAGEDOWN
+
+#define KB_RIGHT			SDLK_RIGHT
+#define KB_LEFT				SDLK_LEFT
+#define KB_DOWN				SDLK_DOWN
+#define KB_UP				SDLK_UP
+
+#define KB_LCTRL			SDLK_LCTRL
+#define KB_LSHIFT			SDLK_LSHIFT
+#define KB_LALT				SDLK_LALT
+#define KB_RCTRL			SDLK_RCTRL
+#define KB_RSHIFT			SDLK_RSHIFT
+#define KB_RALT				SDLK_RALT
+
+#define MOUSEBUTTON_LEFT	SDL_BUTTON_LEFT
+#define MOUSEBUTTON_MIDDLE	SDL_BUTTON_MIDDLE
+#define MOUSEBUTTON_RIGHT	SDL_BUTTON_RIGHT
+#define MOUSEBUTTON_X1		SDL_BUTTON_X1
+#define MOUSEBUTTON_X2		SDL_BUTTON_X2
 
 #endif // PLATFORMER_EVENT_DEFINES_H
